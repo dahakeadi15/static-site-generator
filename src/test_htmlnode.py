c2010031bbc86d61351925dcb80408a505ddfea9
Aditya Dahake <dahakeadi15@gmail.com>
2025-02-10 12:25:05 +0530

    add ParentNode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -95,6 +95,100 @@ class TestLeafNode(unittest.TestCase):
         self.assertEqual(
             node.__repr__(),
             "LeafNode(p, What a strange world, {'class': 'primary'})",
+        )
+
+
+class TestParentNode(unittest.TestCase):
+    def test_repr(self):
+        node = ParentNode("p", [LeafNode("b", "Bold text")], {"class": "info"})
+        self.assertEqual(
+            node.__repr__(),
+            "ParentNode(p, [LeafNode(b, Bold text, None)], {'class': 'info'})",
+        )
+
+    def test_to_html_no_tag(self):
+        node = ParentNode(None, [LeafNode("p", "paragraph")])
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html_no_children(self):
+        node = ParentNode("p", None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html_only_leaves(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+            {"class": "info"},
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<p class="info"><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>',
+        )
+
+    def test_to_html_parent_as_child(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                ParentNode(
+                    "span",
+                    [
+                        LeafNode("i", "italic text"),
+                        LeafNode(None, "Normal text"),
+                    ],
+                ),
+            ],
+            {"class": "info"},
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<p class="info"><b>Bold text</b>Normal text<span><i>italic text</i>Normal text</span></p>',
+        )
+
+    def test_to_html_multiple_parents(self):
+        node = node = ParentNode(
+            "div",
+            [
+                LeafNode("div", "Unordered list"),
+                ParentNode(
+                    "ul", [LeafNode("li", "ul item"), LeafNode("li", "ul item")]
+                ),
+                LeafNode("div", "Ordered list"),
+                ParentNode(
+                    "ol", [LeafNode("li", "ol item1"), LeafNode("li", "ol item2")]
+                ),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<div><div>Unordered list</div><ul><li>ul item</li><li>ul item</li></ul><div>Ordered list</div><ol><li>ol item1</li><li>ol item2</li></ol></div>",
+        )
+
+    def test_to_html_nested_parents(self):
+        node = node = ParentNode(
+            "div",
+            [
+                ParentNode(
+                    "p",
+                    [
+                        LeafNode("b", "Bold text"),
+                        LeafNode(None, "Normal text"),
+                        ParentNode("span", [LeafNode("i", "italic text")]),
+                    ],
+                    {"class": "info"},
+                )
+            ],
+            {"class": "container"},
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<div class="container"><p class="info"><b>Bold text</b>Normal text<span><i>italic text</i></span></p></div>',
         )
 
 
