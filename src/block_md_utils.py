cafe5a3ab43dbb59894b4fdf63843048589fdf4e
Aditya Dahake <dahakeadi15@gmail.com>
2025-02-12 01:20:17 +0530

    markdown document to blocks

---
+++ src/block_md_utils.py
@@ -0,0 +1,3 @@
+def markdown_to_blocks(md_document):
+    blocks = md_document.split("\n\n")
+    return blocks
---
+++ src/test_block_md_utils.py
@@ -0,0 +1,17 @@
+import unittest
+
+from block_md_utils import markdown_to_blocks
+
+
+class TestSplitBlocks(unittest.TestCase):
+    def test_md_to_blocks(self):
+        md_doc = "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+        blocks = markdown_to_blocks(md_doc)
+        self.assertListEqual(
+            blocks,
+            [
+                "# This is a heading",
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+                "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
+            ],
+        )
