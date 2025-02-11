24ae85bd6a9f0f6c8d6040e4099aa2292c555e69
Aditya Dahake <dahakeadi15@gmail.com>
2025-02-12 02:26:03 +0530

    block to block type

--- src/block_md_utils.py
+++ src/block_md_utils.py
@@ -6,3 +6,66 @@ def markdown_to_blocks(md_document):
             continue
         filtered_blocks.append(block.strip())
     return filtered_blocks
+
+
+def block_to_block_type(block: str):
+    # Heading
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return "heading"
+
+    lines = block.split("\n")
+
+    # Code
+    first_line = lines[0]
+    last_line = lines[-1]
+    if first_line.startswith("```") and last_line.startswith("```"):
+        return "code"
+
+    # Quote
+    is_quote = True
+
+    for line in lines:
+        if not line.startswith("> "):
+            is_quote = False
+            break
+
+    if is_quote:
+        return "quote"
+
+    # Unordered List
+    is_ol = True
+
+    for line in lines:
+        if not (line.startswith("* ") or line.startswith("- ")):
+            is_ol = False
+            break
+
+    if is_ol:
+        return "unordered_list"
+
+    # Ordered List
+    is_ul = True
+    prev = 0
+    for line in lines:
+        index = line.split(" ", maxsplit=1)[0]
+        if index[-1] != ".":
+            is_ul = False
+            break
+        curr = int(index[:-1])
+        if curr - 1 != prev:
+            is_ul = False
+            break
+        prev = curr
+
+    if is_ul:
+        return "ordered_list"
+
+    # Normal Paragraph
+    return "paragraph"
--- src/test_block_md_utils.py
+++ src/test_block_md_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_md_utils import markdown_to_blocks
+from block_md_utils import block_to_block_type, markdown_to_blocks
 
 
 class TestSplitBlocks(unittest.TestCase):
@@ -66,6 +66,51 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+
+class TestMdToBlocks(unittest.TestCase):
+    def test_heading_to_block(self):
+        h1 = "# Heading"
+        h2 = "## Heading"
+        h3 = "### Heading"
+        h4 = "#### Heading"
+        h5 = "##### Heading"
+        h6 = "###### Heading"
+        type_1 = block_to_block_type(h1)
+        type_2 = block_to_block_type(h2)
+        type_3 = block_to_block_type(h3)
+        type_4 = block_to_block_type(h4)
+        type_5 = block_to_block_type(h5)
+        type_6 = block_to_block_type(h6)
+        self.assertListEqual(
+            [type_1, type_2, type_3, type_4, type_5, type_6],
+            ["heading"] * 6,
+        )
+
+    def test_code_to_block(self):
+        code = "```\ncode;\nblock;\n# comments\n// more comments\n```"
+        block_type = block_to_block_type(code)
+        self.assertEqual(block_type, "code")
+
+    def test_quote_to_block(self):
+        quote = "> quote first line\n> quote next line\n> - author"
+        block_type = block_to_block_type(quote)
+        self.assertEqual(block_type, "quote")
+
+    def test_ul_to_block(self):
+        ul = "* ul item 1\n- ul item 2"
+        block_type = block_to_block_type(ul)
+        self.assertEqual(block_type, "unordered_list")
+
+    def test_ol_to_block(self):
+        ol = "1. ol item 1\n2. ol item 2\n3. ol item 3"
+        block_type = block_to_block_type(ol)
+        self.assertEqual(block_type, "ordered_list")
+
+    def test_paragraph_to_block(self):
+        para = "normal paragraph\nblock"
+        block_type = block_to_block_type(para)
+        self.assertEqual(block_type, "paragraph")
 
 
 if __name__ == "__main__":
